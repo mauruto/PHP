2147700f46e82ef4cc029a89ffc53ea3a9f41280
Matheus Mauruto Bianchi <matheus.bianchi@farmanet.local>
2018-06-25 09:08:31 -0300

    deletar

--- index.php
+++ index.php
@@ -28,12 +28,16 @@ require_once ("config.php");
 //$aluno->insert();
 //echo $aluno;
 
+//alterar um usuário
+//$usuario = new Usuario();
+//$usuario->loadbyId(8);
+//$usuario->update("professor","!@#$%¨&*");
+//echo $usuario;
+
+
 $usuario = new Usuario();
-
-$usuario->loadbyId(8);
-
-$usuario->update("professor","!@#$%¨&*");
-
+$usuario -> loadbyId(7);
+$usuario->delete();
 echo $usuario;
 
 ?>
